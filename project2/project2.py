95bcae39149f943189ec1e6043870e9ed9f4bfd2
Sean <s_moloney@aol.com>
2017-03-20 20:40:02 -0700

    Weird ass poblem 3

--- project2/project2.py
+++ project2/project2.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 from project import *
 from random import *
 
@@ -23,25 +23,21 @@ def primality3(n, k):
 
 
 def primality2(n, k):
+    print ("Return primality 2")
     a = True
     while(k > 0):
         randInt = randint(2, n-1)
-        (q, r) = primality(dec2bin(randInt), dec2bin(n))
-        if( r != [1]):
+       # print("Randint", randInt)
+        #print ("leaving primality2")
+        r = primality(dec2bin(randInt), dec2bin(n))
+        #print('R', bin2dec(r))
+        
+        #print(q, r)
+        if( bin2dec(r) != 1):
             a = False
         k -=1
     return a
-    '''
-    if(k == 0):
-        return True
-    randInt = randint(2, n-1)
-    (q, r) = primality(dec2bin(randInt), dec2bin(n))
-    print ("Returned from primality")
-    print(q, r)
-    if(r != [1]):
-        return False
-    return primality2(n, k-1)
-    '''
+
 def primality(randInt, n):
     
     return Mod_Exp_Log(randInt,sub(n, [1]), n)
@@ -57,55 +53,88 @@ def div_check(n, a):
         return True
 
     
+def Mod_Exp_Log(x, e, m):
+    X = x
+    E = e
+    Y = [1]
+    while not zero(E):
+       # print("e",bin2dec(E))
+        (p, q) = divide(E,[0,1])
+        if zero(q):
+            val = mult(X, X)
+            (p, q) = divide(val, m)
+            X = q
+            (p, q) = divide(E,[0,1])
+            E = p
+           # print('Y', bin2dec(Y))
+        else:
+            val = mult(X, Y)
+            (p, q) = divide(val, m)
+            Y = q
+            E = sub(E, [1])
+            #print('Y', bin2dec(Y))
+    return Y
+def problem2(n, k):
+    isPrime = False
+    myVector = [1]
+    for i in range (1, n-2):
+        myVector.append(randint(0,1))
+    myVector.append(1)
+    isPrime = primality3(bin2dec(myVector), k)
+    randInt = randint(1, n-1)
+    while not isPrime:
+        randInt = randint(1, n-2)
+        if(myVector[randInt] == 1):
+            myVector[randInt] = 0
+        else:
+            myVector[randInt] = 1
+        isPrime = primality3(bin2dec(myVector), k)
+    return myVector
 
-def Mod_Exp_Log(Base, Exp, Mod):
-    B = Base
-    E = Exp
-    M = Mod
+def problem3(n, k):
+    p = dec2bin(11)#problem2(n, k)
+    q = dec2bin(7) #problem2(n, k)
+    N = mult(p, q)
+    E = problem2(n, k)
     
-    #Mod
-    if (zero(M)):
-        return bin2dec(expo(B, E))
-    if zero(sub(M, [1])):
-        return [0]
+    pq = mult(sub(p, [1]), sub(q, [1]))
+    i = gcd (E, pq)
+    while not bin2dec(i) == 1:
+        print("gcd: ", bin2dec(i))
+        E = problem2(n, k)
+        i = gcd(E, pq)
+    print ('E', bin2dec(E))
+    print('pq', bin2dec(pq))
+    a, b, D = egcd(E, pq)
+    print("a: ", bin2dec(a), "b: ", bin2dec(b), "D: ", bin2dec(D) )
+    print( bin2dec(N), bin2dec(E), bin2dec(D))
+    return (N, E, D)
 
-    #Exponent
-    if zero(E):
-        return ([1], [0])
-    if(compare(E, [1])) == 0:
-        (a, b) = divide(B, M)
-        return (a, b)
-
-    #Base
-    
-    if zero(B):
-        return [0]
-    if zero(sub(B, [1])):
-        return [1]
-    # Math time
-    #Is even?
-    
-    if even(E):
-        (val1, val2) = Mod_Exp_Log(B, divide(E, [0,1]), M)    # Need to clean this up later
-        (q,r1) = divide(val1, M)
-        r1 = mult(r1, r1)
-        (q, r2) = divide (r1, M)
-        return (val1, r2)
+def egcd(a, b):
+    if zero(a):
+        return (b, [0], [1])
     else:
+        p, q = divide(b, a)
+        g, x, y = egcd(q, a)
+        m, r = divide(b, a)
+        k = mult(m, x)
+        l = sub(y, k)
+        return g, l, x
         
-        (val1, val2) = Mod_Exp_Log(B, divide(E, [0,1]), M)   # All of this as well
-        (q, r1) = divide(val1, M)
-        r1 = mult(r1, r1)
-        (q, base2) = divide(B, M)
-        r2 = mult(r1, base2)
-            
-        return divide(r2, M)
-
+              
+    
 def main2():
-    a = primality3(53, 2)
+    # a,b, D = egcd(dec2bin(15), dec2bin(7))
+    #print(bin2dec(a), bin2dec(b), bin2dec(D))
+    # print (bin2dec(a))
+    a = primality3(2,1)
     if(a == False):
         print("Not a prime number.")
     else:
         print("Prime Number.")
+        print(bin2dec(problem2(50, 2)))
+
+    (N, E, D) = problem3(10, 2)
+
 
 main2()
